6985be27e1480ef1c03b1f73ba2f5bcdc40775df
Shashika Theekshana <shashikatheekshana@icloud.com>
2025-07-09 12:59:18 +0530

    Initial Commit

---
+++ CODECRAFT_AD_04( Tic Tac Toe )/CODECRAFT_AD_04__Tic_Tac_Toe__App.swift
@@ -0,0 +1,17 @@
+//
+//  CODECRAFT_AD_04__Tic_Tac_Toe__App.swift
+//  CODECRAFT_AD_04( Tic Tac Toe )
+//
+//  Created by shashika theekshana on BE 2568-07-09.
+//
+
+import SwiftUI
+
+@main
+struct CODECRAFT_AD_04__Tic_Tac_Toe__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CODECRAFT_AD_04( Tic Tac Toe )/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CODECRAFT_AD_04( Tic Tac Toe )
+//
+//  Created by shashika theekshana on BE 2568-07-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
